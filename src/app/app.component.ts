c8a5db826263e59b923d37c3a17a28a564c02c37
Igor Vieira <igor.p.r.vieira@gmail.com>
2019-02-14 15:33:41 -0200

    chore: create the import for modules and components, import packages for modal and toaster and add images for create tag x and O

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit{
+  playerOne: boolean
+  playerTwo: boolean
+  playerNameOne: string
+  playerNameTwo: string
+  players: ['Player 1', 'Player 2']
+  isVisible: boolean
+
+  constructor(public ngxSmartModalService: NgxSmartModalService) {
+  }
+
+  ngOnInit() {
+    this.isVisible = true;
+    this.randomPlayer()
+  }
+
+  randomPlayer() {
+    this.players = ['Player 1', 'Player 2'];
+    const player = this.players[Math.floor(Math.random() * this.players.length)];
+
+    this.playerNameOne = player;
+
+    if(this.playerNameOne === 'Player 1') {
+      this.playerNameTwo = 'Player 2'
+    } else {
+      this.playerNameTwo = 'Player 1'
+    }
+  }
+
+  setHero(hero) {
+    this[hero] = true
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { TicTacToeModule } from './tic-tac-toe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToasterModule } from 'angular2-toaster';
+import { HeroComponent } from './hero/hero.component';
+import { NgxSmartModalModule } from 'ngx-smart-modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    TicTacToeModule,
+    BrowserAnimationsModule,
+    NgxSmartModalModule.forRoot(),
+    ToasterModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
